3905bf8bfa4a1562c42e68681ec203f2ddab7365
Ajeky <alvaromarquez98@gmail.com>
2019-11-22 12:24:53 +0100

    Primer commit. Hecha la primera tarea, comenzada la autenticacion.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD9zYiLRqEqNc5dsCo65YBNphzStUMf58s",
+    authDomain: "scrummanager-7a0ae.firebaseapp.com",
+    databaseURL: "https://scrummanager-7a0ae.firebaseio.com",
+    projectId: "scrummanager-7a0ae",
+    storageBucket: "scrummanager-7a0ae.appspot.com",
+    messagingSenderId: "953326940739",
+    appId: "1:953326940739:web:56da021f032afaf2c5daf6"
+  }
+};
